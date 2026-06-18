20dd1b5ad0c8c0f13ac279b5f7e13eba9420587d
Binita <binitagvasita@gmail.com>
2026-06-18 20:35:59 +0530

    fix: case sensitivity EmailCampaigns

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import BrandKit from './pages/BrandKit'
 import AIGraphics from './pages/AIGraphics'
 import SocialMedia from './pages/SocialMedia'
 import ContentCalendar from './pages/ContentCalendar'
-import EmailCampaigns from './pages/Emailcampaigns'
+import EmailCampaigns from "./pages/EmailCampaigns";
 
 function App() {
   const [user, setUser] = useState(null)
